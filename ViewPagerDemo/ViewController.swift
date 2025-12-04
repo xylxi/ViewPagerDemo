841fdeb598c033f9739063d6a90b4506a09da2e7
xylxi <377793267@qq.com>
2025-12-04 23:15:04 +0800

    init

---
+++ ViewPagerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewPagerDemo
+//
+//  Created by ruirui on 2025/12/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
